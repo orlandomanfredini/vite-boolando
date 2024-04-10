447c4dcf4fb7871ea57c4e21eb495488132c3e41
orlando manfredini <orlando.manfre1@gmail.com>
2024-04-10 17:40:12 +0200

    ADD: components header

--- vite-boolando/src/components/AppHeader.vue
+++ vite-boolando/src/components/AppHeader.vue
@@ -2,9 +2,16 @@
 export default {
     data(){
         return{
-            iconHeader: [
+            currentElement: 0,
+            iconsHeader: [
                 {
-                    icon: '../'
+                    icon: '../public/img/cuore.png'
+                },
+                {
+                    icon: '../public/img/carrello.png'
+                },
+                {
+                    icon: '../public/img/log.png'
                 }
 
             ],
@@ -31,20 +38,20 @@ export default {
 <template>
     <header>
         <div class="container">
-            <div class="row">
+            <div class="row row-header">
                 <div class="col-4">
                     <ul class="link-header">
                         <li v-for="link in linksHeader"><a :href="link.src">{{ link.name }}</a></li>
                     </ul>
                 </div>
-                <div class="col-4">
-                    <div class="logo">
-                        <img :src="logo">
-                    </div>
+                <div class="logo">
+                    <img :src="logo">
                 </div>
                 <div class="col-4">
                     <ul class="icon-header">
-                        <li v-for=""></li>
+                        <li v-for="icon in iconsHeader">
+                            <img :src="icon.icon">
+                        </li>
                     </ul>
                 </div>
             </div>
